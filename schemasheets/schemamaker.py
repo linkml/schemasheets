68a498d4f9df8a7d1b4ec52583b473ba1a228bea
cmungall <cjm@berkeleybop.org>
2022-04-27 18:10:43 -0700

    restored code for n/a to be "-"

--- schemasheets/schemamaker.py
+++ schemasheets/schemamaker.py
@@ -441,6 +441,7 @@ class SchemaMaker:
             pvs = sv.get_enum('Cardinality').permissible_values
             for pv in pvs.values():
                 annv = pv.annotations.get(cvocab)
+                #print(f'CHECKING {card} == {annv}')
                 if card and annv and annv.value == card:
                     matches.append(pv)
                 if card == pv.text:
